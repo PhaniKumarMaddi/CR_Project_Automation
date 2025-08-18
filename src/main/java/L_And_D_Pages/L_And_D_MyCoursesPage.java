0af98a14582ee9efe46149a045f976d08486358d
PhaniKumarMaddi <phani.maddi@criticalriver.com>
2025-08-18 16:27:38 +0530

    created courses page

--- src/main/java/L_And_D_Pages/L_And_D_MyCoursesPage.java
+++ src/main/java/L_And_D_Pages/L_And_D_MyCoursesPage.java
@@ -1,6 +1,28 @@
 package L_And_D_Pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import Utility.DriverManager;
+import Utility.GenerateReports;
+import Utility.TestDataKeys;
+
 public class L_And_D_MyCoursesPage {
 
+	static WebDriver driver;
+	private static Logger logger = LogManager.getLogger(L_And_D_MyCoursesPage.class);
+	GenerateReports grep = new GenerateReports();
+	TestDataKeys dataKeys = new TestDataKeys();
+
+	public L_And_D_MyCoursesPage() {
+		this.driver = DriverManager.getDriver();
+	}
+
+	By coursesHeader = By.cssSelector("div.my-courses>h1");
+	By coursesDesc = By.cssSelector("div.my-courses>p");
+
+	By coursestabs = By.xpath("//div[@class='tabs']/span[text()='All courses']");
+	
 }
- 
